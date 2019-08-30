84db86ae2a6bce9d194fbba20fa84ce05c19486e
Pedro Lemos <pedroemlemos@gmail.com>
2019-08-30 17:34:12 -0300

    E2E with Protractor

--- e2e/src/app.po.ts
+++ e2e/src/app.po.ts
@@ -1,11 +1,51 @@
-import { browser, by, element } from 'protractor';
+import { browser, by, element, ElementFinder } from 'protractor';
 
 export class AppPage {
   navigateTo() {
     return browser.get(browser.baseUrl) as Promise<any>;
   }
 
-  getTitleText() {
-    return element(by.css('app-root h1')).getText() as Promise<string>;
+  getRootComponent(): ElementFinder {
+    return element(by.tagName('app-root'));
+  }
+
+  getWelcomeComponent(): ElementFinder {
+    return this.getRootComponent().element(by.tagName('app-welcome'));
+  }
+
+  getMonitorComponent(): ElementFinder {
+    return this.getRootComponent().element(by.tagName('app-monitor'));
+  }
+
+  getPragmaTitle(): Promise<string> {
+    return this.getWelcomeComponent().element(by.css('h2')).getText() as Promise<string>;
+  }
+
+  getTemperatureMonitor(): Promise<string> {
+    return this.getWelcomeComponent().element(by.css('h3')).getText() as Promise<string>;
+  }
+
+  getTruckIdInput(): ElementFinder {
+    return this.getWelcomeComponent().element(by.css('input'));
+  }
+
+  sendKeysTruckIdInput(id: string): Promise<void> {
+    return this.getTruckIdInput().sendKeys(id) as Promise<void>;
+  }
+
+  getCheckIcon(): ElementFinder {
+    return this.getWelcomeComponent().element(by.className('check'));
+  }
+
+  getTimesIcon(): ElementFinder {
+    return this.getWelcomeComponent().element(by.className('times'));
+  }
+
+  getMonitorButton(): ElementFinder {
+    return this.getWelcomeComponent().element(by.buttonText('Monitor'));
+  }
+
+  getTile(): ElementFinder {
+    return element(by.css('app-temperature'));
   }
 }
